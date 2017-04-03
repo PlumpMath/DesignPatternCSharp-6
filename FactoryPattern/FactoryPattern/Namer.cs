12bee01822cb6a96da978f1910bf122474d63fa6
levj1 <levj1@yahoo.com>
2017-04-03 16:26:02 -0400

    Create factory pattern
    factory pattern

---
+++ FactoryPattern/FactoryPattern/Namer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class Namer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class FirstFirst: Namer
+    {
+        public FirstFirst(string name)
+        {
+            int i = name.IndexOf(" ");
+            if (i > 0)
+            {
+                FirstName = name.Substring(0, i).Trim();
+                LastName = name.Substring(i + 1).Trim();
+            }
+            else
+            {
+                LastName = name;
+                FirstName = "";
+            }
+        }
+    }
+
+    public class LastFirst : Namer
+    {
+        public LastFirst(string name)
+        {
+            int i = name.IndexOf(",");
+            if(i > 0)
+            {
+                LastName = name.Substring(0, 1);
+                FirstName = name.Substring(i + 1).Trim();
+            }
+            else
+            {
+                LastName = name;
+                FirstName = "";
+            }
+        }
+    }
+}
